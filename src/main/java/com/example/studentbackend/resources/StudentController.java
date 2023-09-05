83361c42f41d52ee1e28cbddeb6d0624d425bd36
wallace <wallace.silva35@fatec.sp.gov.br>
2023-09-05 14:26:20 -0300

    ajustes

--- src/main/java/com/example/studentbackend/resources/StudentController.java
+++ src/main/java/com/example/studentbackend/resources/StudentController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,6 +24,7 @@ import com.example.studentbackend.services.StudentService;
 
 @RestController
 @RequestMapping("students")
+@CrossOrigin
 public class StudentController {
     @Autowired
     private StudentService service;
